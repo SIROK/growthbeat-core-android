2794c8c8e9dd89decb92ebeb50d24996947a9d78
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-06-30 23:04:08 +0900

    Implement ClientObserver interface

--- source/src/com/growthbeat/Growthbeat.java
+++ source/src/com/growthbeat/Growthbeat.java
@@ -1,15 +1,19 @@
 package com.growthbeat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.growthbeat.http.HttpClient;
 import com.growthbeat.model.Client;
+import com.growthpush.observer.ClientObserver;
 
 public class Growthbeat {
 
 	private static final Growthbeat instance = new Growthbeat();
 	private static final String DEFAULT_BASE_URL = "http://api.localhost:8085/";
 
-	private String secret;
 	private Client client;
+	private List<ClientObserver> clientObservers = new ArrayList<ClientObserver>();
 
 	private Growthbeat() {
 		HttpClient.getInstance().setBaseUrl(DEFAULT_BASE_URL);
@@ -21,23 +25,56 @@ public class Growthbeat {
 
 	public void initialize(final String applicationId, final String secret) {
 
-		this.secret = secret;
-
-		Logger.getInstance().info(String.format("initialize (applicationId:%s)", applicationId));
-
 		new Thread(new Runnable() {
 
 			@Override
 			public void run() {
-				Growthbeat.this.client = Client.create(applicationId, Growthbeat.this.secret);
-				Logger.getInstance().info(String.format("client created (id:%s)", Growthbeat.this.client.getId()));
+
+				Logger.getInstance().info(String.format("Initializing... (applicationId:%s)", applicationId));
+
+				client = Preference.getInstance().fetchClient();
+				if (client != null && client.getApplication().getId().equals(applicationId)) {
+					Logger.getInstance().info(String.format("Client already exists. (id:%s)", client.getId()));
+					update(client);
+					return;
+				}
+
+				// TODO clear preference
+
+				Logger.getInstance().info(String.format("Creating client... (applicationId:%s)", applicationId));
+				client = Client.create(applicationId, secret);
+
+				if (client == null) {
+					Logger.getInstance().info("Failed to create client.");
+					return;
+				}
+
+				Preference.getInstance().saveClient(client);
+				Logger.getInstance().info(String.format("lient created. (id:%s)", client.getId()));
+				update(client);
+
 			}
+
 		}).start();
 
 	}
 
 	public Client getClient() {
 		return client;
+	}
+
+	public void addClientObserver(ClientObserver clientObserver) {
+		clientObservers.add(clientObserver);
+	}
+
+	public void removeClientObserver(ClientObserver clientObserver) {
+		clientObservers.remove(clientObserver);
+	}
+
+	public void update(Client client) {
+		for (ClientObserver clientObserver : clientObservers) {
+			clientObserver.update(client);
+		}
 	}
 
 }
---
+++ source/src/com/growthpush/observer/ClientObserver.java
@@ -0,0 +1,9 @@
+package com.growthpush.observer;
+
+import com.growthbeat.model.Client;
+
+public interface ClientObserver {
+
+	public void update(Client client);
+
+}
