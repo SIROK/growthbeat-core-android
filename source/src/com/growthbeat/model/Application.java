3a8f340e39288013d7ff2ded4a1bbaa00ea89286
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-07-01 20:59:48 +0900

    Export jar package and link in sample project

--- source/src/com/growthbeat/model/Application.java
+++ source/src/com/growthbeat/model/Application.java
@@ -1,13 +1,10 @@
 package com.growthbeat.model;
 
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import com.growthbeat.http.HttpClient;
 import com.growthbeat.utils.DateUtils;
 import com.growthbeat.utils.JSONObjectUtils;
 
