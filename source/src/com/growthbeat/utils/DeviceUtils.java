2bb419369db5a241439bbc462394642ffcca6541
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2014-08-08 19:58:53 +0900

    Add device status fetching method to DeviceUtils

--- source/src/com/growthbeat/utils/DeviceUtils.java
+++ source/src/com/growthbeat/utils/DeviceUtils.java
@@ -3,7 +3,14 @@ package com.growthbeat.utils;
 import java.util.Locale;
 import java.util.TimeZone;
 
+import android.app.ActivityManager;
+import android.content.Context;
+import android.graphics.Point;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo.State;
 import android.os.Build;
+import android.view.Display;
+import android.view.WindowManager;
 
 public final class DeviceUtils {
 
@@ -33,6 +40,48 @@ public final class DeviceUtils {
 
 	public static String getTimeZone() {
 		return TimeZone.getDefault().getID();
+	}
+
+	public static boolean connectedToWiFi(Context context) {
+		ConnectivityManager connectivityManager = SystemServiceUtils.getConnectivityManager(context);
+		try {
+			State wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState();
+			if (wifi == State.CONNECTED || wifi == State.CONNECTING)
+				return true;
+		} catch (Exception e) {
+		}
+
+		return false;
+	}
+
+	public static long getAvailableMemory(Context context) {
+		ActivityManager activityManager = SystemServiceUtils.getActivityManager(context);
+		ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+		activityManager.getMemoryInfo(memoryInfo);
+		return memoryInfo.availMem;
+	}
+
+	public static boolean isLowMemory(Context context) {
+		ActivityManager activityManager = SystemServiceUtils.getActivityManager(context);
+		ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+		activityManager.getMemoryInfo(memoryInfo);
+		return memoryInfo.lowMemory;
+	}
+
+	@SuppressWarnings("deprecation")
+	public static Point getDisplaySize(Context context) {
+		WindowManager windowManager = SystemServiceUtils.getWindowManager(context);
+		Point point = new Point();
+		Display display = windowManager.getDefaultDisplay();
+
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2)
+			display.getSize(point);
+		else {
+			point.x = display.getWidth();
+			point.y = display.getHeight();
+		}
+
+		return point;
 	}
 
 }
