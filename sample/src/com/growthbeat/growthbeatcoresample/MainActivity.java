0b871976b2ea7b4d1b33a64ac2651145e717dcf0
Naoyuki Kataoka <kataokanaoyuki@gmail.com>
2015-04-07 06:57:49 +0900

    Implement sample of intent handling

--- sample/src/com/growthbeat/growthbeatcoresample/MainActivity.java
+++ sample/src/com/growthbeat/growthbeatcoresample/MainActivity.java
@@ -4,16 +4,22 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.growthbeat.GrowthbeatCore;
 import com.growthbeat.model.Client;
+import com.growthbeat.model.NoopIntent;
+import com.growthbeat.model.UrlIntent;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
@@ -22,6 +28,24 @@ public class MainActivity extends Activity {
 			}
 		}).start();
 		GrowthbeatCore.getInstance().initialize(this.getApplicationContext(), "OyVa3zboPjHVjsDC", "3EKydeJ0imxJ5WqS22FJfdVamFLgu7XA");
+
+		findViewById(R.id.url_intent).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				UrlIntent urlIntent = new UrlIntent();
+				urlIntent.setUrl("https://growthbeat.com/");
+				GrowthbeatCore.getInstance().handleIntent(urlIntent);
+			}
+		});
+
+		findViewById(R.id.noop_intent).setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				NoopIntent noopIntent = new NoopIntent();
+				GrowthbeatCore.getInstance().handleIntent(noopIntent);
+			}
+		});
+
 	}
 
 	@Override
